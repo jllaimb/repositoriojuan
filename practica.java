b87cfc86b8dc381fb17a2e0824bdad70bdb2702d
Juan Antonio Llamas Imbernon <jllaimb@g.educaand.es>
2022-05-12 13:10:11 +0200

    Version1.0

---
+++ practica.java
@@ -0,0 +1,17 @@
+package prueba;
+
+public class practica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola Mundo");
+		
+		System.out.println(Hola a todos);
+		
+		
+		
+
+	}
+
+}
